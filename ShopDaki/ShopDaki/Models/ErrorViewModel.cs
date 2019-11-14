1528cb64b88a7a51bcfb9c4832a9735796a6d094
Kiet Tran <44864496+radiun42@users.noreply.github.com>
2019-11-14 16:47:35 +0700

    Khởi Tạo Project
    - Khởi tạo website bán hàng.

---
+++ ShopDaki/ShopDaki/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShopDaki.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
