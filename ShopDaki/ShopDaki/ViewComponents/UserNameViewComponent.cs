2e960453a0c3a33c9d6c8fef6539b0db724c10be
Kiet Tran <44864496+radiun42@users.noreply.github.com>
2019-11-30 18:28:17 +0700

    Tạo View Component
    - Thay đổi hiển thị tên tài khoản thành tên người dùng.
    - Tạo cơ chế bảo mật 2 bước.

---
+++ ShopDaki/ShopDaki/ViewComponents/UserNameViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShopDaki.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace ShopDaki.ViewComponents
+{
+    public class UserNameViewComponent: ViewComponent
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserNameViewComponent(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var claims = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            var userFromDb = await _db.ApplicationUsers.Where(m => m.Id == claims.Value).FirstOrDefaultAsync();
+
+            return View(userFromDb);
+        }
+    }
+}
