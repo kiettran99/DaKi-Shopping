150f0ddd1e84e2b51a4897194557729013da2312
Kiet Tran <44864496+radiun42@users.noreply.github.com>
2019-12-03 20:06:52 +0700

    Thêm Trang ProductsList
    - Thêm trang products list và hiện tại tiếp tục làm lọc trang sản phẩm.
    - Sửa lỗi không thể cấp quyền tài khoản super admin.

---
+++ ShopDaki/ShopDaki/Areas/Customers/Controllers/ProductsListController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShopDaki.Data;
+using ShopDaki.Models;
+using ShopDaki.Models.ViewModel;
+
+namespace ShopDaki.Areas.Customers.Controllers
+{
+    [Area("Customers")]
+    public class ProductsListController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProductsListViewModel ProductsListVM { get; set; }
+
+        private int PageSize = 1;
+
+        public ProductsListController(ApplicationDbContext db)
+        {
+            _db = db;
+            ProductsListVM = new ProductsListViewModel()
+            {
+                Products = new List<Product>()
+            }; 
+        }
+
+        public async Task<IActionResult> Index(int productPage = 1, string searchName = null)
+        {
+            ProductsListVM.Products = await _db.Products.Include(m => m.GroupProduct).ToListAsync();
+
+            StringBuilder param = new StringBuilder();
+            param.Append("/Customers/ProductsList?productPage=:");
+
+            if (searchName != null)
+            {
+                param.Append("&searchName=");
+                param.Append(searchName);
+            }
+
+            if (searchName != null)
+            {
+                ProductsListVM.Products = ProductsListVM.Products.Where(p => p.Name.ToLower().Trim().Contains(searchName.ToLower().Trim())).ToList();
+            }
+
+            var count = ProductsListVM.Products.Count();
+
+            ProductsListVM.Products = ProductsListVM.Products.OrderBy(p => p.Date)
+                .Skip((productPage - 1) * PageSize).Take(PageSize).ToList();
+
+            ProductsListVM.PagingInfo = new PagingInfo()
+            {
+                CurrentPage = productPage,
+                ItemsPerPage = PageSize,
+                TotalItems = count,
+                urlParam = param.ToString()
+            };
+
+            return View(ProductsListVM);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
--- ShopDaki/ShopDaki/Data/DbInitializer.cs
+++ ShopDaki/ShopDaki/Data/DbInitializer.cs
@@ -9,42 +9,44 @@ using System.Threading.Tasks;
 
 namespace ShopDaki.Data
 {
-    public class DbInitializer :IDbInitializer
+    public class DbInitializer : IDbInitializer
     {
         private readonly ApplicationDbContext _db;
         private readonly UserManager<IdentityUser> _userManager;
-        private readonly RoleManager<IdentityUser> _roleManager;
-        
-        public DbInitializer(ApplicationDbContext db, UserManager<IdentityUser>userManager,RoleManager<IdentityUser>roleManager)
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public DbInitializer(ApplicationDbContext db, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
         {
             _db = db;
-            _roleManager = roleManager;
             _userManager = userManager;
+            _roleManager = roleManager;
         }
         public async void initialize()
         {
-            if(_db.Database.GetPendingMigrations().Count()>0)
+            if (_db.Database.GetPendingMigrations().Count() > 0)
             {
                 _db.Database.Migrate();
             }
-            _db.Database.Migrate();
-            if (_db.Roles.Any(r => r.Name == SD.SuperAdminEndUser))return;
+
+            if (_db.Roles.Any(r => r.Name == SD.SuperAdminEndUser)) return;
+
             _roleManager.CreateAsync(new IdentityRole(SD.AdminEndUser)).GetAwaiter().GetResult();
             _roleManager.CreateAsync(new IdentityRole(SD.SuperAdminEndUser)).GetAwaiter().GetResult();
+            _roleManager.CreateAsync(new IdentityRole(SD.Salesman)).GetAwaiter().GetResult();
+            _roleManager.CreateAsync(new IdentityRole(SD.Customer)).GetAwaiter().GetResult();
 
-            _userManager.CreateAsync(new ApplicationUser
+            var userAdmin = new ApplicationUser()
             {
                 UserName = "daki@gmail.com",
                 Email = "daki@gmail.com",
-                Name = "daki",
+                Name = "Super Admin",
                 EmailConfirmed = true
+            };
 
-            }, "daki1@").GetAwaiter().GetResult();
-            IdentityUser user = await _db.Users.Where(u => u.Email == "daki@gamil.com").FirstOrDefaultAsync();
-            await _userManager.AddToRoleAsync(await _userManager.FindByEmailAsync("daki@gmail.com"), SD.SuperAdminEndUser);
+            var resultUser = _userManager.CreateAsync(userAdmin, "Daki123@").GetAwaiter().GetResult();
 
-
+            await _userManager.AddToRoleAsync(userAdmin, SD.SuperAdminEndUser);
         }
-             
+
     }
 }
