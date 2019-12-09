2ad88b01a66d314948b57e5f3ecfdc10b853f718
Kiet Tran <44864496+radiun42@users.noreply.github.com>
2019-12-09 21:18:38 +0700

    Tạo Thống kê Hóa Đơn
    - Có thể tạo thống kế số lượng hóa đơn theo tháng.
    - Tạo biểu tượng mess cố định giúp liên lạc với nhân viên.
    - Sửa giao diện Products ist.
    - Tiếp tục kiểm tra lỗi không thể cấp quyền Super Admin khi chưa có cơ sở dữ liệu.

---
+++ ShopDaki/ShopDaki/Areas/Admin/Controllers/OrderStatisticsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ShopDaki.Models;
+using ShopDaki.Models.ViewModel;
+
+namespace ShopDaki.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OrderStatisticsController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public OrderStatisticsViewModel OrderStatisticVM { get; set; }
+
+        public OrderStatisticsController(ApplicationDbContext db)
+        {
+            _db = db;
+
+            OrderStatisticVM = new OrderStatisticsViewModel()
+            {
+                Orders = new List<Order>()
+            };
+        }
+
+        public IActionResult Index(int SelectedYear = 0)
+        {
+            OrderStatisticVM.Orders = _db.Orders.ToList();
+
+            if (SelectedYear != 0)
+            {
+                OrderStatisticVM.SelectedYear = SelectedYear;
+            }
+
+            return View(OrderStatisticVM);
+        }
+    }
+}
---
+++ ShopDaki/ShopDaki/Models/ViewModel/OrderStatisticsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopDaki.Models.ViewModel
+{
+    public class OrderStatisticsViewModel
+    {
+        public List<Order> Orders { get; set; }
+        public int SelectedYear { get; set; }
+
+        public OrderStatisticsViewModel()
+        {
+            SelectedYear = DateTime.Now.Year;
+        }
+    }
+}
